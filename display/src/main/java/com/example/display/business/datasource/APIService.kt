15cd6b7291476bf0f2aa0aa125fa87f8105d88e4
David PL <david.2289@icloud.com>
2021-03-03 05:06:44 +0100

    Api call unit test

--- display/src/main/java/com/example/display/business/datasource/APIService.kt
+++ display/src/main/java/com/example/display/business/datasource/APIService.kt
@@ -1,12 +1,21 @@
 package com.example.display.business.datasource
 
 import com.example.display.business.model.Users
+import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
+import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface APIService {
 
     @GET("api/users")
     fun fetchUsers(): Single<Users>
+
+    @GET("api/users")
+    fun fetchUsersObservable(): Observable<Response<Users>>
+
+    @GET("api/users")
+    fun fetchUsersCall(): Call<Users>
 
 }
